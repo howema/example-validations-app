b4ebdcd2a2294638ef6ab3a5f81a2e0c995b91db
MEG HOWE <howem9@gmail.com>
2021-06-23 15:05:34 -0400

    Updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,18 @@ class UsersController < ApplicationController
              status: :unprocessable_entity
     end
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.first_name = params[:first_name] || user.first_name
+    user.last_name = params[:last_name] || user.last_name
+    user.email = params[:email] || user.email
+
+    if user.save
+      render json: user.as_json
+    else
+      render json: { errors: users.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
 end
